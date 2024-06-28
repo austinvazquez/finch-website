12dd203246305ff5dd09c311c30f22e2ecf0c695
Austin Vazquez <macedonv@amazon.com>
2024-06-28 07:56:30 -0700

    chore(python): format code
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- scripts/get_upstream_docs.py
+++ scripts/get_upstream_docs.py
@@ -7,7 +7,7 @@ import requests
 api_url = "https://api.github.com/repos/runfinch/finch/releases/latest"
 response = requests.get(api_url)
 data = response.json()
-zip_file_url = data['zipball_url']
+zip_file_url = data["zipball_url"]
 r = requests.get(zip_file_url)
 
 z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -21,7 +21,7 @@ with open("../docs/docs/changelog.md", "wb") as f:
 
 for file_name in namelist:
     doc_file_prefix = namelist[0] + "docs/cmd/"
-    if file_name.startswith(doc_file_prefix) and file_name[-1] != '/':
+    if file_name.startswith(doc_file_prefix) and file_name[-1] != "/":
         rest = file_name.split(doc_file_prefix)[1]
         with open(f"../docs/docs/cli-reference/{rest}", "wb") as f:
             f.write(z.read(file_name))
